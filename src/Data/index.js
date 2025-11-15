ddc71c7a6682b68164ca5e6cf68debe02be44bf9
jubayar <eng.jubayar@gmail.com>
2025-11-15 17:11:47 +0600

    Add profile card

--- src/Data/index.js
+++ src/Data/index.js
@@ -1,24 +1,36 @@
 export const profileCardData = [
   {
-    image: 'assets/logo-colored.svg',
-    name: 'bongoDev',
-    about: 'To empower tech enthusiasts in Bangladesh.',
-    background: 'bg-orange-200',
+    image: "assets/logo-colored.svg",
+    name: "bongoDev",
+    about: "To empower tech enthusiasts in Bangladesh.",
+    background: "bg-orange-200",
     socials: {
-      linkedin: 'https://linkedin.com/in/bongodev',
-      github: 'https://github.com/bongodev',
-      portfolio: 'https://bongodev.com/',
+      linkedin: "https://linkedin.com/in/bongodev",
+      github: "https://github.com/bongodev",
+      portfolio: "https://bongodev.com/",
     },
   },
   {
-    image: 'assets/girl.png',
-    name: 'Sumiya Yasmin',
-    about: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit',
-    background: 'bg-blue-200',
+    image: "assets/jubayar.webp",
+    name: "Jubayar Ahamad",
+    about:
+      "I’m passionate about learning, solving problems, and creating applications that make an impact.",
+    background: "bg-purple-200",
     socials: {
-      linkedin: 'https://linkedin.com/in/sumiyayasmin',
-      github: 'https://github.com/sumiyayasmin',
-      portfolio: 'https://sumiyayasmin.com',
+      linkedin: "https://www.linkedin.com/in/jubayar-ahamad-find/",
+      github: "https://github.com/yet-yuvi",
+      portfolio: "https://jubayar-portfolio.netlify.app/",
+    },
+  },
+  {
+    image: "assets/girl.png",
+    name: "Sumiya Yasmin",
+    about: "Neque porro quisquam est qui dolorem ipsum quia dolor sit",
+    background: "bg-blue-200",
+    socials: {
+      linkedin: "https://linkedin.com/in/sumiyayasmin",
+      github: "https://github.com/sumiyayasmin",
+      portfolio: "https://sumiyayasmin.com",
     },
   },
 ];
