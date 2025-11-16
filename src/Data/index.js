88b985408235ec01bba56c7063d6a1e7d18ee892
jubayar <eng.jubayar@gmail.com>
2025-11-16 16:48:19 +0600

    yet-yuvi: Fix issues, Update card pic source, Format code

--- src/Data/index.js
+++ src/Data/index.js
@@ -11,18 +11,6 @@ export const profileCardData = [
     },
   },
   {
-    image: "assets/jubayar.webp",
-    name: "Jubayar Ahamad",
-    about:
-      "I’m passionate about learning, solving problems, and creating applications that make an impact.",
-    background: "bg-purple-200",
-    socials: {
-      linkedin: "https://www.linkedin.com/in/jubayar-ahamad-find/",
-      github: "https://github.com/yet-yuvi",
-      portfolio: "https://jubayar-portfolio.netlify.app/",
-    },
-  },
-  {
     image: "assets/girl.png",
     name: "Sumiya Yasmin",
     about: "Neque porro quisquam est qui dolorem ipsum quia dolor sit",
@@ -31,6 +19,19 @@ export const profileCardData = [
       linkedin: "https://linkedin.com/in/sumiyayasmin",
       github: "https://github.com/sumiyayasmin",
       portfolio: "https://sumiyayasmin.com",
+    },
+  },
+  {
+    image:
+      "https://media.licdn.com/dms/image/v2/D4E03AQF8ZPLn2OkrCQ/profile-displayphoto-crop_800_800/B4EZjYox2aHEAM-/0/1755981226507?e=1764806400&v=beta&t=kjWYBC_qARPER_ibe_xb0l12wRZa0aAodE_3QviYkEE",
+    name: "Jubayar Ahamad",
+    about:
+      "I’m passionate about learning, solving problems, and creating applications that make an impact.",
+    background: "bg-rose-200",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/jubayar-ahamad-find/",
+      github: "https://github.com/yet-yuvi",
+      portfolio: "https://jubayar-portfolio.netlify.app/",
     },
   },
 ];
